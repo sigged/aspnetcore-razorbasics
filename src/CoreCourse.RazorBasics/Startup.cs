c4987ada8833a5e663405b081099517a826efb72
Bart Soete <bart.soete@howest.be>
2021-07-10 12:41:41 +0200

    Add Services in StartUp

--- src/CoreCourse.RazorBasics/Startup.cs
+++ src/CoreCourse.RazorBasics/Startup.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Builder;
+﻿using CoreCourse.RazorBasics.Domain.Services;
+using CoreCourse.RazorBasics.Services;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
@@ -23,7 +25,10 @@ namespace CoreCourse.RazorBasics
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            
             services.AddControllersWithViews();
+            services.AddTransient<IMazeDecorationService, GrayMazeDecorationService>();
+            services.AddTransient<INewsArticleService, NewsArticleFakeService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
